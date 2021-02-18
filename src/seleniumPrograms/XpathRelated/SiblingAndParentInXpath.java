5c855fb885528f332b45cc444d16fcdd9e863928
leninyenni <myseleniumwork@gmail.com>
2021-02-18 21:55:19 +0530

    Changes on Feb 18,2021

--- src/seleniumPrograms/XpathRelated/SiblingAndParentInXpath.java
+++ src/seleniumPrograms/XpathRelated/SiblingAndParentInXpath.java
@@ -4,8 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
-import seleniumPrograms.BaseClass;
+
 import seleniumPrograms.HandlingPopUps.UsingJavaScriptExecutor;
 
 import java.util.List;
