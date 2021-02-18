96e4845335f7f2282d56772143cb766ef9a98b6a
leninyenni <myseleniumwork@gmail.com>
2021-02-18 21:54:15 +0530

    Changes on Feb 18,2021

---
+++ src/seleniumPrograms/XpathRelated/SiblingAndParentInXpath.java
@@ -0,0 +1,37 @@
+package seleniumPrograms.XpathRelated;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import seleniumPrograms.BaseClass;
+import seleniumPrograms.HandlingPopUps.UsingJavaScriptExecutor;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class SiblingAndParentInXpath extends UsingJavaScriptExecutor {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver",
+                "C://Users//91939//drivers//chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/automation-practice-form");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://demo.guru99.com/test/guru99home/");
+        //Search element inside 'Popular course' which are sibling of control 'SELENIUM' ,Here first we will find a h2 whose text is ''A few of our most popular courses' ,then we move to its parent element which is a 'div' , inside this div we will find a link whose text is 'SELENIUM' then at last we will find all of the sibling elements of this link('SELENIUM')
+        List<WebElement> dateBox = driver.findElements(By.xpath("//h2[contains(text(),'A few of our most popular courses')]/parent::div//div[//a[text()='SELENIUM']]/following-sibling::div[@class='rt-grid-2 rt-omega']"));
+        //Print all the which are sibling of the the element named as 'SELENIUM' in 'Popular course'
+        for (WebElement webElement : dateBox) {
+            scrollIntoView(webElement,driver);
+            drawBorder(webElement, driver);
+            System.out.println(webElement.getText());
+        }
+        driver.close();
+    }
+}
+
+
+
