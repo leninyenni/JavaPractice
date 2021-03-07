22b87ff48abe93a8059802423d464aad1b863ac2
leninyenni <myseleniumwork@gmail.com>
2021-03-07 20:08:29 +0530

    Changes till Mar 7, 2021

---
+++ src/telegramSource/InterviewPrograms/StringCombs.java
@@ -0,0 +1,17 @@
+package telegramSource.InterviewPrograms;
+
+public class StringCombs {
+    public static void main(String[] args) {
+        String str = "ABCD";
+        //a,b,c,d,ab,bc,cd,abc,bcd,abcd
+            for (int i = 0; i < str.length(); i++) {
+            for (int j = 1; j <= str.length(); j++) {
+                if (i < j) {
+                    System.out.println(str.substring(i, j));
+                }
+            }
+
+        }
+    }
+}
+
